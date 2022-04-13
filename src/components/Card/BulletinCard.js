4a2decb473aa299725888a5671c1ba02ce070b90
skfcb10@naver.com <skfcb10@naver.com>
2022-04-13 23:58:12 +0900

    CHORE Edit styles of context view

--- src/components/Card/BulletinCard.js
+++ src/components/Card/BulletinCard.js
@@ -1,11 +1,12 @@
 //게시글 카드
-
 import React from "react";
 import { FaThumbsUp, FaRegThumbsUp } from "react-icons/fa";
 import api from "api/api";
 import useCurrentUser from "Hooks/useCurrentUser";
 import Loading from "Contents/Loading";
 import { useHistory } from "react-router-dom";
+import "./BulletinCard.scss";
+
 const BulletinCard = ({ data, type, onThumb }) => {
   // console.log("bulletinCard", type, d);
   const history = useHistory();
@@ -57,27 +58,33 @@ const BulletinCard = ({ data, type, onThumb }) => {
   if (data) {
     console.log(data);
     return (
-      <div>
-        <div>글내용:{data.content}</div>
-        <div>글 쓴 사람:{data.user.nickName}</div>
-        {/* 이거 쓰고 싶은데 안된다... 왜 안될까 */}
-
-        <div>쓰여진 날짜{data.registerDate}</div>
-        <div>글 쓴 사람이 준 별점:{data.score && data.score}</div>
-        <div>좋아요 수:{data.thumbCount}</div>
-
-        <div>
-          내가 이 글에 대해 좋아요를 눌렀는지?
-          <button
-            onClick={() => handleThumb(data)}
-            style={{ cursor: "pointer" }}
-          >
-            {data.thumbJudge ? <FaThumbsUp /> : <FaRegThumbsUp />}
-          </button>
+      <div className="bulletinCard">
+        <div className="bulletinCard__header">
+          <div className="bulletinCard__header__img">
+            <img src={data.book.image} alt="" height="280" width="180"></img>
+          </div>
+          <div className="bulletinCard__header__title">{data.book.title}</div>
+          <div>이 책을 읽은 <strong>{data.user.nickName}</strong>님의 {type==="review" ? "리뷰" : "필사"}</div>
         </div>
-        {data.user.id === currentUser.id && (
-          <button onClick={() => deleteContext(data)}>삭제</button>
-        )}
+        <div className="bulletinCard__body">
+          <div>글내용:{data.content}</div>
+          {/* 이거 쓰고 싶은데 안된다... 왜 안될까 */}
+          <div>{data.registerDate}</div>
+          {type==="review"&&<div>{data.score && data.score}</div>}
+          <div>좋아요 수:{data.thumbCount}</div>
+
+          <div>
+            <button
+              onClick={() => handleThumb(data)}
+              style={{ cursor: "pointer" }}
+            >
+              {data.thumbJudge ? <FaThumbsUp /> : <FaRegThumbsUp />}
+            </button>
+          </div>
+          {data.user.id === currentUser.id && (
+            <button onClick={() => deleteContext(data)}>삭제</button>
+          )}
+        </div>
       </div>
     );
   }
--- src/pages/Detail/TransDetail.js
+++ src/pages/Detail/TransDetail.js
@@ -1,60 +1,3 @@
-// import React, { useCallback, useEffect, useState } from "react";
-// import TransList from "components/List/TransList";
-// import TransRegister from "../../components/Card/TransRegister";
-// import useCurrentUser from "Hooks/useCurrentUser";
-// import api from "api/api";
-
-// const TransDetail = ({ isbn, title }) => {
-//   const { currentUser } = useCurrentUser();
-//   const [otherTrans, setOtherTrans] = useState([]);
-//   const [transPage, setTransPage] = useState(0);
-//   const [totalTransPage, setTotalTransPage] = useState(0);
-//   const [inView, setInView] = useState(false);
-//   const [loading, setLoading] = useState(false); //trans
-//   const handleThumb = () => {
-//     handleTransExist();
-//     setTransPage(totalTransPage / 5 + 1);
-//   };
-//   const handleTransExist = useCallback(async () => {
-//     setLoading(true);
-//     const response = await api.get(
-//       `transcription/inquiry/${currentUser.id}/${isbn}/${transPage}`
-//     );
-//     //검색
-//     let preData = [];
-//     if (response.data.totalSize > 0) {
-//       response.data.list.forEach((item) => {
-//         preData.push(item);
-//       });
-//       setOtherTrans([...otherTrans, ...preData]);
-//     }
-//     setTotalTransPage(response.data.totalSize);
-//     setLoading(false);
-//   }, [transPage]);
-//   useEffect(() => {
-//     // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니라면
-//     if (inView && !loading) {
-//       setTransPage(transPage + 1);
-//     }
-//   }, [inView, loading]);
-//   useEffect(() => {
-//     handleTransExist();
-//   }, [handleTransExist]);
-//   return (
-//     <div className="detail__trans">
-//       <TransList
-//         data={otherTrans}
-//         onThumb={handleThumb}
-//         title={title}
-//         setInView={setInView}
-//         totalTransPage={totalTransPage}
-//       />
-//       <TransRegister isbn={isbn} update={handleTransExist} />
-//     </div>
-//   );
-// };
-
-// export default TransDetail;
 import BulletinCard from "components/Card/BulletinCard";
 import React, { useEffect, useState } from "react";
 import "./index.scss";
@@ -62,6 +5,8 @@ import { useHistory } from "react-router-dom";
 import useCurrentUser from "Hooks/useCurrentUser";
 import api from "api/api";
 import Loading from "Contents/Loading";
+import "./TransDetail.scss";
+
 const TransDetail = ({ location, match }) => {
   const { currentUser } = useCurrentUser();
   const history = useHistory();
